f20e67d7db6ef6ab1ea71a2ab3d68a74a1321f00
gustavo <gustavo.castilho@ufabc.edu.br>
2018-11-09 17:11:51 -0200

    Alterações menores; Nivel 1 adequado ao Scripts/StageManager.cs

---
+++ Assets/Scripts/Blink_Iniciar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink_Iniciar : MonoBehaviour {
+
+    // this is the UI.Text or other UI element you want to toggle
+    public MaskableGraphic imageToToggle;
+
+    public float interval = 1f;
+    public float startDelay = 0.5f;
+    public bool currentState = true;
+    public bool defaultState = true;
+    bool isBlinking = false;
+
+    //public AudioClip clip;
+
+    void Start()
+    {
+        imageToToggle.enabled = defaultState;
+        StartBlink();
+    }
+
+    public void StartBlink()
+    {
+        // do not invoke the blink twice - needed if you need to start the blink from an external object
+        if (isBlinking)
+            return;
+
+        if (imageToToggle != null)
+        {
+            isBlinking = true;
+            InvokeRepeating("ToggleState", startDelay, interval);
+        }
+    }
+
+    public void ToggleState()
+    {
+        imageToToggle.enabled = !imageToToggle.enabled;
+
+        // plays the clip at (0,0,0)
+        //if (clip)
+        //    AudioSource.PlayClipAtPoint(clip, Vector3.zero);
+    }
+
+
+}

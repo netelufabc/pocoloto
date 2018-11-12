305667745bd5a9f51f1ad73521ffa49b9b34e17e
gustavo <gustavo.castilho@ufabc.edu.br>
2018-11-12 15:54:42 -0200

    //

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//
+
 public class SoundManager : MonoBehaviour {
 
     private static SoundManager instance = null;

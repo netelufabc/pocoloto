1eda349aa8c1b7f3a3580defeb89793a248b3700
Andre <jomonzito2010@gmail.com>
2018-12-20 09:39:26 -0200

    Classe Player.cs que contém as informações do jogador para serializar

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Player
+{
+    public int key;
+    public string nome;
+    public bool genero;
+    public int idade;
+    public int serie;
+
+}

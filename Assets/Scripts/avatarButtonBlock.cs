6ffbc360e1fe8532f4a7cac89d0ed4398a02d2a1
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-01-08 14:57:36 -0200

    Bugfixes
    Cliques em qualquer outro lugar desabilitam os botões de confirmar e deletar nas telas 03.2_loadSelection e 04_avatarSelect

--- Assets/Scripts/avatarButtonBlock.cs
+++ Assets/Scripts/avatarButtonBlock.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class avatarButtonBlock : MonoBehaviour, IPointerClickHandler
 {
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        GameObject.Find("Confirma").GetComponent<UnityEngine.UI.Button>().interactable = false;
+        if (eventData.pointerPress.tag != "AvatarButton" || !eventData.pointerPress.GetComponent<UnityEngine.UI.Button>().interactable)
+        {
+            GameObject.Find("Confirma").GetComponent<UnityEngine.UI.Button>().interactable = false;
+        }
     }
 }

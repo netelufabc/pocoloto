373516fd2258dc2a78ee5e8e447f9cb7c32ccdf2
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-01-15 09:04:15 -0200

    P1A2 OK

--- Assets/Scripts/ButtonsScripts/ButtonConfirmar.cs
+++ Assets/Scripts/ButtonsScripts/ButtonConfirmar.cs
@@ -64,6 +64,7 @@ public class ButtonConfirmar : MonoBehaviour
 
     public void ConfirmaResposta()
     {
+        timer.ResetTimeProgressBar();
         StartCoroutine(ConfirmandoResposta());
     }
 

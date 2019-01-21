4d0b9419a356ffa6e1457b975c20b23732dea480
Andre <jomonzito2010@gmail.com>
2019-01-21 09:11:33 -0200

    Update de players

--- Assets/Scripts/SaveScripts/Player.cs
+++ Assets/Scripts/SaveScripts/Player.cs
@@ -14,56 +14,47 @@ public class Player
     public int avatarSelecionadoIndex = -1;
     public bool[] avatarBloqueado = new bool[10] { false, false, true, true, true, true, true, true, true, true };
     public bool[] sistemaLiberado = new bool[5] { true, false, false, false, false };
-    public int[,] pontuacao = new int[20, 3]; //Ler como planeta x, ato y
 
-    public void CriarPontuacaoInicial()
+    public Row[] planeta;
+
+    public void CriatPontuacao()
     {
-        for (int i = 0; i<20; i++)
-        {
-            for (int j = 0; j<3; j++)
-            {
-                pontuacao[i, j] = 0;
-            }
-        }
-    }
-
-    public Row[] pontuacaoTeste;
-
-    public void TesteCriarPontuacao()
-    {
-        pontuacaoTeste = new Row[22];
-        pontuacaoTeste[0].initRow(4);
-        pontuacaoTeste[1].initRow(4);
-        pontuacaoTeste[2].initRow(4);
-        pontuacaoTeste[3].initRow(2);
-        pontuacaoTeste[4].initRow(4);
-        pontuacaoTeste[5].initRow(4);
-        pontuacaoTeste[6].initRow(4);
-        pontuacaoTeste[7].initRow(4);
-        pontuacaoTeste[8].initRow(4);
-        pontuacaoTeste[9].initRow(4);
-        pontuacaoTeste[10].initRow(4);
-        pontuacaoTeste[11].initRow(4);
-        pontuacaoTeste[12].initRow(4);
-        pontuacaoTeste[13].initRow(4);
-        pontuacaoTeste[14].initRow(4);
-        pontuacaoTeste[15].initRow(4);
-        pontuacaoTeste[16].initRow(4);
-        pontuacaoTeste[17].initRow(4);
-        pontuacaoTeste[18].initRow(4);
-        pontuacaoTeste[19].initRow(4);
-        pontuacaoTeste[20].initRow(4);
-        pontuacaoTeste[21].initRow(4);
+        planeta = new Row[22];
+        planeta[0].initRow(4);
+        planeta[1].initRow(4);
+        planeta[2].initRow(4);
+        planeta[3].initRow(2);
+        planeta[4].initRow(4);
+        planeta[5].initRow(4);
+        planeta[6].initRow(4);
+        planeta[7].initRow(4);
+        planeta[8].initRow(4);
+        planeta[9].initRow(4);
+        planeta[10].initRow(4);
+        planeta[11].initRow(4);
+        planeta[12].initRow(4);
+        planeta[13].initRow(4);
+        planeta[14].initRow(4);
+        planeta[15].initRow(4);
+        planeta[16].initRow(4);
+        planeta[17].initRow(4);
+        planeta[18].initRow(4);
+        planeta[19].initRow(4);
+        planeta[20].initRow(4);
+        planeta[21].initRow(4);
     }
 }
 
 [Serializable]
 public struct Row
 {
-    public int[] rowData;
+    public int[] ato;
 
     public void initRow(int i)
     {
-        rowData = new int[i];
+        ato = new int[i];
     }
+
+    ///Ato 0 indica quantos atos existem no planeta
+    ///acessar como planeta[x].ato[y];
 }

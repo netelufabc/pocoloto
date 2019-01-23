7edfadfbd9c3847ddc6d8413192f1d6298d2623c
Andre <jomonzito2010@gmail.com>
2019-01-23 12:07:09 -0200

    Tela de load corrigida.

---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ToggleButton : MonoBehaviour {
+
+    Button button;
+    public static ColorBlock defaulColor;
+
+    private void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(BlankAllTheOthers);
+        defaulColor = button.colors;
+    }
+
+    public void BlankAllTheOthers()
+    {
+        foreach (Button button in Button.allSelectables)
+        {
+            if (button.CompareTag("Button"))
+            {
+                button.colors = defaulColor;
+            }
+        }
+        ChangeColor();
+    }
+
+    void ChangeColor()
+    {
+        ColorBlock cb = button.colors;
+        cb.normalColor = button.colors.highlightedColor; ///Highlight e variaveis
+        button.colors = cb;
+    }
+}
--- Assets/Scripts/loadButtonBlock.cs
+++ Assets/Scripts/loadButtonBlock.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 // Utilizando eventos - IPointerClickHandler - para verificar ocorrência de clicks do mouse na tela
@@ -15,5 +16,12 @@ public class loadButtonBlock : MonoBehaviour, IPointerClickHandler {
     {
         GameObject.Find("Confirma").GetComponent<UnityEngine.UI.Button>().interactable = false;
         GameObject.Find("Delete").GetComponent<UnityEngine.UI.Button>().interactable = false;
+        foreach (Button button in Button.allSelectables)
+        {
+            if (button.CompareTag("Button"))
+            {
+                button.colors = ToggleButton.defaulColor;
+            }
+        }
     }
 }

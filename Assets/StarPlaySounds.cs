108c6287fb74dc1036f00d1b454b0bb50f297723
gustavo <gustavo.castilho@ufabc.edu.br>
2019-01-25 16:55:45 -0200

    Adicionados sons nos feedbacks de estrelas obtidas.

---
+++ Assets/StarPlaySounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarPlaySounds : MonoBehaviour {
+
+    AudioClip firstAndSecondStars;
+    AudioClip thirdStar;
+    SoundManager soundManager;
+
+    private void Awake()
+    {
+        soundManager = SoundManager.instance;
+        firstAndSecondStars = (AudioClip)Resources.Load("Sounds/sfx/estrela1");
+        thirdStar = (AudioClip)Resources.Load("Sounds/sfx/estrela2");
+    }
+
+    public void PlayOneAndTwoStarsSound()
+    {
+        soundManager.PlaySfx(firstAndSecondStars);
+    }
+
+    public void PlayThreeStarsSound()
+    {
+        soundManager.PlaySfx(thirdStar);
+    }
+
+}

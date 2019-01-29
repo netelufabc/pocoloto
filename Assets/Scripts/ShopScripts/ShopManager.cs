adbeb2ad8eec190f47033e0474924712f478b386
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-01-29 16:16:42 -0200

    Esqueleto da scene da loja

---
+++ Assets/Scripts/ShopScripts/ShopManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Linq;
+
+public class ShopManager : MonoBehaviour {
+
+    private ToggleGroup toggleGroup;
+    public Sprite[] avatarsToSell;
+    public Color[] colorsToSell;
+    public GameObject[] extrasToSell;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ Assets/Scripts/ShopScripts/ShopToggleWindow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ShopToggleWindow : MonoBehaviour, IPointerClickHandler {
+
+    private ToggleGroup toggleGroup;
+
+    // Use this for initialization
+    void Start () {
+        toggleGroup = FindObjectOfType<ToggleGroup>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerClick(PointerEventData pointerClick)
+    {
+        string tempToggleName = toggleGroup.ActiveToggles().FirstOrDefault().name;
+
+        tempToggleName = string.Concat(tempToggleName, "Selected");
+
+        Invoke(tempToggleName, 0);
+    }
+
+    private void AvatarToggleSelected()
+    {
+        Debug.Log("Avatar");
+    }
+
+    private void CoresToggleSelected()
+    {
+        Debug.Log("Cores");
+    }
+
+    private void ExtrasToggleSelected()
+    {
+        Debug.Log("Extras");
+    }
+}

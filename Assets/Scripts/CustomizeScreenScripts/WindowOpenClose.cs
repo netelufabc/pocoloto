e1efd6980888cf8f481f69569199588ac4b1fc8e
Andre <jomonzito2010@gmail.com>
2019-02-01 15:40:05 -0200

    Mudar de avatar/cor salva o jogo.

--- Assets/Scripts/CustomizeScreenScripts/WindowOpenClose.cs
+++ Assets/Scripts/CustomizeScreenScripts/WindowOpenClose.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class WindowOpenClose : MonoBehaviour {
 
     public GameObject window;
+    public bool save;
 
 	public void CloseFather()
     {
+        SaveGame();
         Destroy(this.transform.parent.gameObject);
     }
 
     public void OpenWindow()
     {
         Instantiate(window, GameObject.Find("Canvas").transform);
+    }
+
+    public void SaveGame()
+    {
+        if (save)
+        {
+            SaveManager.Save();
+        }
     }
 }

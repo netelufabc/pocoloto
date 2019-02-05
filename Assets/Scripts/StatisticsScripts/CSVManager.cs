6d1216e7fce90a377a089a151a7daa6d7dc3f80b
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-02-05 16:30:36 -0200

    Princípio das estatísticas

---
+++ Assets/Scripts/StatisticsScripts/CSVManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CSVManager : MonoBehaviour {
+
+    public StatisticsData statisticsData;
+    private string statisticsDataPath;
+    private string fileHeaderCSV = "NumSistema,NumPlaneta,RespostaCorreta,PalavraSelecionada,PalavraEscrita,Tempo,UtilizouDicaisual,UtiizouDicaAuditiva\n";
+    private string resumeHeader = "NumSistema,NumPlaneta,Passou,Acertos,Erros,TempoTotal\n";
+
+    private void Awake()
+    {
+        statisticsDataPath = Application.persistentDataPath + "/" + SaveManager.player.nome + ".csv";
+        if (!File.Exists(statisticsDataPath))
+        {
+            StreamWriter sw = File.CreateText(statisticsDataPath);
+            sw.Close();
+            File.AppendAllText(statisticsDataPath, fileHeaderCSV);
+        }
+    }
+
+    public void SaveStatistics(StatisticsData data)
+    {
+        if (File.Exists(statisticsDataPath))
+        {
+            File.AppendAllText(statisticsDataPath, data.ToString());
+        }
+        else
+        {
+            Debug.Log("Arquivo não encontrado");
+        }
+    }
+
+    public void AddResume()
+    {
+        if (!File.Exists(statisticsDataPath))
+        {
+            File.AppendAllText(statisticsDataPath, resumeHeader);
+        }
+    }
+
+    public void TestScript()
+    {
+        SaveStatistics(statisticsData);
+    }
+}
---
+++ Assets/Scripts/StatisticsScripts/StatisticsData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class StatisticsData {
+
+    public string sistema;
+    public string planeta;
+    public string palavraSelecionada;
+    public string palavraEscrita;
+    public bool respostaCorreta;
+    public bool dicaVisual;
+    public bool dicaAuditiva;
+    public float tempoUtiizado;
+
+    private string lineSeparator = "\n";
+    private string fieldSeparator = ",";
+
+    public override string ToString()
+    {
+        return sistema + fieldSeparator + planeta + fieldSeparator + respostaCorreta + fieldSeparator +
+            palavraSelecionada + fieldSeparator + palavraEscrita + fieldSeparator + tempoUtiizado + fieldSeparator +
+            dicaVisual + fieldSeparator + dicaAuditiva + lineSeparator;
+    }
+}

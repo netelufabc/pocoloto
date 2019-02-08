4937748350be11de45fe9e6e35d4c4422b6b7558
gustavo <gustavo.castilho@ufabc.edu.br>
2019-02-08 15:00:28 -0200

    Tela créditos e ajustes menores.

---
+++ Assets/Credits.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+    SoundManager soundManager;
+    AudioClip creditsSong;
+    bool isFading;
+
+	// Use this for initialization
+	void Start () {
+        creditsSong = Resources.Load("Sounds/Music/random silly chip song") as AudioClip;
+        soundManager = SoundManager.instance;
+        soundManager.PlayBackground(creditsSong);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            if (!isFading)
+            {
+                gameObject.AddComponent<LoadScene>();
+                gameObject.GetComponent<LoadScene>().LoadSceneWithFade("02_mainMenu");
+                isFading = true;
+            }
+        }
+	}
+}
